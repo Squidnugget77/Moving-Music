d56063b9e28ad7baa55003e437c25eac027a73c2
Luca G <luca.p.giannotti@gmail.com>
2022-08-14 21:31:01 -0500

    Worked on applemusic_xl

--- applemusic_xl.py
+++ applemusic_xl.py
@@ -2,13 +2,24 @@
 import xml.etree.ElementTree as ET
 tree = ET.parse('Library.xml')
 root = tree.getroot
-
 #   Finds the name of the song {}
 def find_name():
-    pass
+    key = 'Name'
+    tags = root().iter()
+    songs = []
+    for elem in tags:
+        if key == elem.text:
+            next_node = next(tags)
+            songs.append(next_node.text)
 
+    print(songs)
+
+        
+
+    
 
 #   Find the artist of the song {}
 
 #   Find the album name {}
 
+find_name()
